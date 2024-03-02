2b763920f240f9b76ab6cf7de200aae2d1928abb
Nathan Metens <nathanmetens1@gmail.com>
2024-03-01 22:40:23 -0800

    Added a linear-linked-list folder with a node.h file

---
+++ linear-linked-list/node.h
@@ -0,0 +1,8 @@
+// This header file contains a public structure called "node".
+// The node structure has two variables; one is an integer 
+// and the other is a pointer to another struct.
+
+struct node {
+	int data;
+	node* next;
+};
